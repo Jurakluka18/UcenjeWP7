144d929756c62dcc4de3f4af3f43c7c3533e65fd
Jurakluka18 <mana2411@gmail.com>
2024-11-27 20:18:31 +0100

    kreiranje tablice smjerovi

--- skriptawp7.sql
+++ skriptawp7.sql
@@ -1,7 +1,19 @@
-﻿--create database edunovawp7;
+﻿
 
---use edunovawp7;
+use master;
+go
+drop database if exists edunovawp7;
+go
+create database edunovawp7;
+go
+use edunovawp7;
+go
 
-create table smjer(
-naziva varchar(50)
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+trajanje int,
+cijena decimal(18,2),
+vaucer bit,
+izvodiseod datetime
 );
